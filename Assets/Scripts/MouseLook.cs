0a54f3df33cd1bd7642e4d3962a6d2f5dac7c8de
ErdemAkz <akz.erdem@gmail.com>
2023-02-01 22:13:22 +0300

    Klavye Mouse kontrolleri eklendi
    VR kontrolleri harici olarak klavye mouse girdileri ile karakter hareket ettirilebilir ve etrafa bakabilir.
    Assets altında prefabs klasörüne paket halinde yerleştirilmiştir.
    Ayrıca denemek için Scenes klasörü altında basit bir harita dizayn edilmiştir.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensivity = 100f;
+    public Transform playerBody;
+    //float xRotation = 0f;
+
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    [SerializeField] Transform cam;
+    [SerializeField] Transform orientation;
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Time.timeScale == 0f) return;
+
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensX * multiplier;
+        xRotation -= mouseY * sensY * multiplier;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+        
+
+    }
+}
