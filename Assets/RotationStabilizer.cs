ba24a2705aa7f1a5a673759fbc34f23a1e620c48
ErdemAkz <akz.erdem@gmail.com>
2023-04-07 15:40:11 +0300

    character fix

--- Assets/RotationStabilizer.cs
+++ Assets/RotationStabilizer.cs
@@ -13,8 +13,6 @@ public class RotationStabilizer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 rotation = this.transform.parent.transform.rotation.eulerAngles;
-        rotation *= -1;
         this.transform.rotation = Quaternion.Euler(0,0,0);
     }
 }
