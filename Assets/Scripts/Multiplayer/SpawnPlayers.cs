acb70c5366e3e419a5d9dfe91e970d09136c28d1
Hasan Erdem AK <70165677+hasanerdemak@users.noreply.github.com>
2023-04-20 11:29:47 +0300

    Menü şablonu değiştirildi

--- Assets/Scripts/Multiplayer/SpawnPlayers.cs
+++ Assets/Scripts/Multiplayer/SpawnPlayers.cs
@@ -5,9 +5,10 @@ using Photon.Pun;
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject playerPrefab;
+    public Transform[] spawnPoints;
 
     void Start()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, this.transform.position, Quaternion.identity);
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoints[JoinToRoom.spawnIndex].position, Quaternion.identity);
     }
 }
